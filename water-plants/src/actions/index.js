96bf1f5e54feae317ba334337707cd501bac146f
Jeremy Morales <jeremy.morales@gmail.com>
2019-07-11 18:59:55 -0700

    added missing success/failure types

--- water-plants/src/actions/index.js
+++ water-plants/src/actions/index.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import axiosWithAuth from './axiosWithAuth';
 
 export const LOGIN_START = 'LOGIN_START';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 
 export const login = creds => dispatch => {
   dispatch({ type: LOGIN_START });
