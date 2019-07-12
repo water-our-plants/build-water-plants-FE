212b597477e988555e3ade280d0adf1861dd795d
Israel Gonzalez <israelgonzalez@lacroizrules.lan>
2019-07-11 18:33:00 -0700

    Initial commit of this branch. Building out registration component with redux thunk

---
+++ water-plants/src/actions/index.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+export const REGISTER_USER_START = "REGISTER_USER_START";
+export const REGISTER_USER_SUCCESS = "REGISTER_USER_SUCCESS";
+export const REGISTER_USER_FAIL = "REGISTER_USER_FAIL";
+
+export const registerUser = ({ username, password, phoneNumber }) => {
+  return dispatch => {
+    dispatch(registerUserStart());
+
+    axios
+      .post(`https://be-water-my-plants.herokuapp.com/api/register`, {
+        username,
+        password,
+        phoneNumber,
+        completed: false
+      })
+      .then(res => {
+        dispatch(registerUserSuccess(res.data));
+      })
+      .catch(err => {
+        dispatch(registerUserFail(err.message));
+      });
+  };
+};
+
+const registerUserSuccess = user => ({
+  type: REGISTER_USER_SUCCESS,
+  payload: {
+    ...user
+  }
+});
+
+const registerUserStart = () => ({
+  type: REGISTER_USER_START
+});
+
+const registerUserFail = error => ({
+  type: REGISTER_USER_FAIL,
+  payload: {
+    error
+  }
+});
