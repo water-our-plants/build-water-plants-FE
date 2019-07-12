8e36a17fc0899151c29324d15a88c9c6d68c3366
Jeremy Morales <jeremy.morales@gmail.com>
2019-07-11 20:15:55 -0700

    login functional with tokenset to localstorage

--- water-plants/src/actions/index.js
+++ water-plants/src/actions/index.js
@@ -10,11 +10,12 @@ export const login = creds => dispatch => {
   return axios
     .post('https://be-water-my-plants.herokuapp.com/api/login', creds)
     .then(res => {
-      localStorage.setItem('token', res.data.payload);
+      console.log(res);
+      localStorage.setItem('token', res.data.token);
       dispatch({ type: LOGIN_SUCCESS });
     })
     .catch(err => {
       console.log(err);
-      dispatch({ type: LOGIN_FAILURE, payload: err.response });
+      dispatch({ type: LOGIN_FAILURE, payload: err.response.data.message });
     });
 };
