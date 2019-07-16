7a16cf4af24d354321d39e701e42d13f40455e91
Israel Gonzalez <israelgonzalez@lacroizrules.lan>
2019-07-15 20:05:33 -0700

    Registration form running on registration route

--- water-plants/src/components/Register.js
+++ water-plants/src/components/Register.js
@@ -5,15 +5,74 @@ import { connect } from "react-redux";
 
 import { registerUser } from "../actions";
 
-const Register = props => {
-  const createUser = e => {
-    props.registerUser({username: "testUser2", password: "password1", phoneNumber: 1234567890});
+class Register extends React.Component {
+  state = {
+    registration: { 
+      username: "", 
+      password: "", 
+      phoneNumber: "" 
+    }
+  };
+
+ handleChange = e => {
+    this.setState({
+      registration: {
+      ...this.state.registration,
+      [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  handleRegistration = e => {
+    e.preventDefault();
+    console.log("Registration button clicked");
+    this.props
+      .registerUser(this.state.registration)
+      .then(() => this.props.history.push("/"));
+  };
+
+  createUser = e => {
+    e.preventDefault();
+    registerUser(this.state.registration);
+    console.log("registered");
+  };
+
+ 
+  render() {
+    return (
+      <>
+        <form>
+          <input
+            name="username"
+            placeholder="Username..."
+            type="text"
+            value={this.state.registration.username}
+            onChange={this.handleChange}
+          />
+          <input
+            name="password"
+            placeholder="Password..."
+            type="password"
+            value={this.state.registration.password}
+            onChange={this.handleChange}
+          />
+          <input
+            name="phoneNumber"
+            placeholder="Phone number..."
+            type="text"
+            value={this.state.registration.phoneNumber}
+            onChange={this.handleChange}
+          />
+          <button
+            type="button"
+            onClick={this.handleRegistration}
+          >
+            Register
+          </button>
+        </form>
+      </>
+    );
   }
-  return (
-    <>
-      <button type="button" onClick={createUser}>Click to register</button>
-    </>
-  )
 }
 
 const mapStateToProps = state => ({
